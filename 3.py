7a88e98ad029ab9a7ef005463db6483ebd3c1b51
fajar6110118 <fajarbudigrolier@gmail.com>
2020-09-14 04:28:12 +0700

    tugas1

---
+++ 3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 15:33:07 2020
+
+@author: mfbd
+"""
+
+import numpy as np
+
+def mse_loss(y_true, y_pred):
+  # y_true and y_pred are numpy arrays of the same length.
+  return ((y_true - y_pred) ** 2).mean()
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred)) # 0.5
